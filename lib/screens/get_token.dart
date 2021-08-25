986f73010463ee694c085495573a23f55f9858a6
ahsam <ahsammmm@gmail.com>
2021-08-25 13:04:31 +0500

    token_status.dart, doctors_duty.dart and services.dart working with api

--- lib/screens/get_token.dart
+++ lib/screens/get_token.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:queue_app/values.dart';
-import 'package:http/http.dart' as http;
+// import 'package:http/http.dart' as http;
 
 class GetToken extends StatelessWidget {
   @override
